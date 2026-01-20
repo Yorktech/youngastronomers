867ab722ad1a1c2811585a1571c3cc81c66b54ef
Steve <steve@yorktechsolutions.co.uk>
2026-01-20 08:55:14 +0000

    mobile tweak

--- components/SlideDeck.js
+++ components/SlideDeck.js
@@ -2,9 +2,9 @@
 
 export default function SlideDeck({ slides }) {
     return (
-        <div className="snap-y snap-mandatory h-screen w-screen h-[100dvh] overflow-y-scroll scroll-smooth bg-transparent text-white">
+        <div className="snap-y snap-mandatory h-screen w-full h-[100dvh] overflow-y-scroll scroll-smooth bg-transparent text-white">
             {slides.map((slide, index) => (
-                <div key={index} className="snap-start h-screen h-[100dvh] w-screen flex flex-col items-center justify-center p-4 md:p-8 relative border-b border-white/10">
+                <div key={index} className="snap-start min-h-[100dvh] w-full flex flex-col items-center justify-center p-6 md:p-8 py-20 relative border-b border-white/10">
                     <Slide content={slide.content} meta={slide.meta} />
                 </div>
             ))}
